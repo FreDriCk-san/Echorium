1072da496730e0050d1b9c807bd8ff9a43ab5244
FreDriCk <ferdinonand@gmail.com>
2022-06-22 00:15:31 +0700

    0:15 Commit -> Created empty template

---
+++ Echorium/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace Echorium.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ Echorium/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Echorium.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
