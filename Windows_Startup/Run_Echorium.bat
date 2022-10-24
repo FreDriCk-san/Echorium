7d674e2ee9d10bf97b3b1bac7a3aed88af2edaea
FreDriCk <ferdinonand@gmail.com>
2022-10-24 19:56:30 +0700

    19:55 Commit -> Created scripts for Windows startup

---
+++ Windows_Startup/Run_Echorium.bat
@@ -0,0 +1,19 @@
+:: Move to upper folder
+echo "/---------------------Move to upper folder--------------------/"
+cd ..
+
+:: Move to main project
+echo "/---------------------Move to main project--------------------/"
+cd Echorium
+
+:: Clean project files
+echo "/---------------------Clean project files--------------------/"
+dotnet clean
+
+:: Build project files
+echo "/---------------------Build project files--------------------/"
+dotnet build
+
+:: Run program
+echo "/---------------------Run program--------------------/"
+dotnet run
